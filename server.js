dfbf189514fead40ff7482038a3fd34af3ac1f5a
zayneDevelops <126347157+zayneDevelops@users.noreply.github.com>
2023-12-23 18:56:16 -0600

    Fixed cors errors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express() // parentheses means we're using express and storing it inside of the variable 'app'
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
 const rappers = {
     '21 savage': {
